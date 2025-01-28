0df4d85121eea0234d5a162ec24799fb65eff81e
Kyle Xiang <cxiang@lcs.on.ca>
2025-01-28 12:07:01 -0500

    Organized project structure.

--- Todolist/Model/TodolistApp.swift
+++ Todolist/Model/TodolistApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TodolistApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LandingView()
         }
     }
 }
---
+++ Todolist/Views/LandingView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todolist
+//
+//  Created by Chujun Xiang on 2025-01-28.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    LandingView()
+}
