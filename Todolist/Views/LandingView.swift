3b0f2e252a3c5c7a839127409afc6ce74aaa0614
Kyle Xiang <cxiang@lcs.on.ca>
2025-01-28 12:16:26 -0500

    A simple list is presented showing the to-do items. Also added a navigation title

--- Todolist/Views/LandingView.swift
+++ Todolist/Views/LandingView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct LandingView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                List{
+                    Text("Study for Chemistry quiz")
+                    Text("Finish Computer Science assignment")
+                    Text("Go for a run around campus")
+                }
+            }
+            .navigationTitle("To do")
+            .padding()
         }
-        .padding()
     }
 }
 
