2875748e2b1ff94203e99cc8926534a7a21d52ad
Kyle Xiang <cxiang@lcs.on.ca>
2025-01-28 12:02:17 -0500

    Initial Commit

---
+++ Todolist/TodolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodolistApp.swift
+//  Todolist
+//
+//  Created by Chujun Xiang on 2025-01-28.
+//
+
+import SwiftUI
+
+@main
+struct TodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
